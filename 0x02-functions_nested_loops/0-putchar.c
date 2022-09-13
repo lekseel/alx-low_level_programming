abe1ff95d63e9ef27bad1a03817e649470a4c9cb
lekseel <olalekanshogbaike@gmail.com>
2022-09-13 14:33:59 -0700

    nothing but good

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	_putchar("\n");
+	_putchar('\n');
 
 return (0);
 }
