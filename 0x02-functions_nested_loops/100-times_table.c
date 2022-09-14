813de79c12fd5c092fcac481bf027195c9e5e5fc
lekseel <olalekanshogbaike@gmail.com>
2022-09-13 18:26:54 -0700

    wonderful

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,99 +1,47 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
- * print_times_table - print the n times table
- * @n: is the integer to be used for the times table
- *
-=======
-=======
->>>>>>> a3b9aa5692fea82ea4ed397d568fd8fefe89cb81
- * print_times_table - prints the times table
- * @n: integer for which the times table will be printed
- *
- * Description: prints the times table
-<<<<<<< HEAD
->>>>>>> 47866c651cdf1bf15a66ee21993171a80c32c9ba
-=======
-=======
- * print_times_table - print the n times table
- * @n: is the integer to be used for the times table
- *
->>>>>>> 9fca2a3b0261aa2ebf2ba32e74f99e48f8338a9d
->>>>>>> a3b9aa5692fea82ea4ed397d568fd8fefe89cb81
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
-
 void print_times_table(int n)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-int row, column, product;
+	int i, j, k;
 
-if (n >= 0         if (n >= 0         if (n >= 0 && n < 15)        if (n >= 0 && n < 15) n < 15)        if (n >= 0         if (n >= 0 && n < 15)        if (n >= 0 && n < 15) n < 15) n < 15)
-=======
-	int row, column, product;
-
-	if (n >= 0 && n < 15)
->>>>>>> 47866c651cdf1bf15a66ee21993171a80c32c9ba
-=======
-	int row, column, product;
-
-	if (n >= 0 && n < 15)
-=======
-int row, column, product;
-
-if (n >= 0         if (n >= 0         if (n >= 0 && n < 15)        if (n >= 0 && n < 15) n < 15)        if (n >= 0         if (n >= 0 && n < 15)        if (n >= 0 && n < 15) n < 15) n < 15)
->>>>>>> 9fca2a3b0261aa2ebf2ba32e74f99e48f8338a9d
->>>>>>> a3b9aa5692fea82ea4ed397d568fd8fefe89cb81
+	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (i = 0; i <= n; i++)
 		{
-			for (column = 0; column <= n; column++)
+			for (j = 0; j <= n; j++)
 			{
-				product = row * column;
-
-				if (column == 0)
-					_putchar('0');
-				else if (product < 10)
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(product % 10 + '0');
-				}
-				else if (product >= 10 && product < 100)
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else if (product > 99 && product < 1000)
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
 		}
 	}
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 47866c651cdf1bf15a66ee21993171a80c32c9ba
-=======
-
-
-=======
->>>>>>> 9fca2a3b0261aa2ebf2ba32e74f99e48f8338a9d
->>>>>>> a3b9aa5692fea82ea4ed397d568fd8fefe89cb81
