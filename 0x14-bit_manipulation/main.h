49474204e99516088049a3f49fd40dbda391e902
lekseel <olalekanshogbaike@gmail.com>
2022-10-27 11:24:52 -0700

    manipulating

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,19 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*
+ * File: holberton.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int _putchar(char c);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int check_valid_string(const char *b);
 
 #endif
+git add .
