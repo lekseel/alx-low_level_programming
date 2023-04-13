f31e130b525df6b28e009755b031d31bde6bf4f6
lekseel <olalekanshogbaike@gmail.com>
2023-04-13 11:27:36 -0700

    search the algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,60 +1,34 @@
-#include "search_algos.h"
+#include <stdio.h>
 
-/**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int recursive_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+    if (array == NULL) {
+        return -1;
+    }
 
-	index = recursive_search(array, size, value);
+    int low = 0;
+    int high = size - 1;
 
-	if (index >= 0 && array[index] != value)
-		return (-1);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        printf("Searching in array: ");
+        for (size_t i = low; i <= high; i++) {
+            printf("%d", array[i]);
+            if (i != high) {
+                printf(", ");
+            }
+        }
+        printf("\n");
 
-	return (index);
+        if (array[mid] == value) {
+            return mid;
+        } else if (array[mid] < value) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
